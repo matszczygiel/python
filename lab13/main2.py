2c636a47e89fc414a81ef4201c8073c1c7c3b9af
Mateusz <mateusz.szczygiel@student.uw.edu.pl>
2019-01-23 17:33:36 +0100

    done

--- lab13/main2.py
+++ lab13/main2.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import trapz
 
 
 beta2 = 0.5
@@ -11,14 +12,17 @@ k = 3
 def D(x):
     return -beta2 * x**2
 
-def one_step(dz, D_function, g, tvals, dt, psi):
+def V(x):
+    return 10 * x**2
+
+def one_step(dz, D_function, g, tvals, dt, psi, pot):
     omegas = np.fft.fftfreq(tvals.shape[-1], dt) * 2 * np.pi
     
     ft = np.fft.fft(psi, norm="ortho")
     ft *= np.exp(-1j * D_function(1j*omegas) * dz / 2.)
     psi_half = np.fft.ifft(ft, norm="ortho")
 
-    ft = np.fft.fft(psi_half * np.exp(-1j * g * np.abs(psi_half)**2 * dz), norm="ortho")
+    ft = np.fft.fft(psi_half * np.exp(-1j *( g * np.abs(psi_half)**2)* dz), norm="ortho")
     ft *= np.exp(-1j * D_function(1j*omegas) * dz / 2.)
     return np.fft.ifft(ft, norm="ortho")
 
@@ -28,7 +32,10 @@ def U(tvals, k):
 
 N = 1000
 tt, dt = np.linspace(-10, 10, num = N, retstep=True)
-psi0 = np.exp(-4* tt**2 )
+#psi0 = np.exp(-4* tt**2 )
+psi0 = U(tt, k)
+
+norm = np.sqrt(trapz(psi0**2, tt))
 
 psiAm = U(tt, k)
 
@@ -38,9 +45,9 @@ for i in range(1000):
     print(i)
     plt.clf()
     if i != 0:
-        psi0 = one_step(1j*dz, D, g, tt, dt, psi0)
+        psi0 = one_step(1j*dz, D, g, tt, dt, psi0, V)
 
-    psi0 *= psiAm[int(N/2)] / psi0[int(N/2)]
+    psi0 *= norm / np.sqrt(trapz(np.abs(psi0)**2, tt))
     #psiA = np.exp(1j * k * i * dz) * U(tt, k)
     
     plt.ylim((0, 3))
@@ -52,6 +59,8 @@ for i in range(1000):
     #plt.plot(tt, psiA.real, 'x', markersize=0.6)
     #plt.plot(tt, psiA.imag, 'x', markersize=0.6)
     plt.plot(tt, psiAm, 'x', markersize=0.6)
+
+    #plt.plot(tt, V(tt))
 
     nStr = str(i)
     nStr=nStr.rjust(3, '0')
